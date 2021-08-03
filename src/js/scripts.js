d0cc4dffe23832376e7076a62a058737d10603b5
Mehul <mehul@zealousweb.com>
2021-08-03 16:41:25 +0530

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -43,117 +43,12 @@ $(document).ready(function () {
 		}
 	});
 
-	// image carousel
-	$('.image-carousel').slick({
-		infinite: true,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		dots: true,
-		responsive: [
-			{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 767,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 640,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-	});
-
-	// highlights carousel
-	$('.hc_large').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		fade: true,
-		asNavFor: '.hc_thumb'
-	});
-	$('.hc_thumb').slick({
-	slidesToShow: 4,
-	slidesToScroll: 1,
-	asNavFor: '.hc_large',
-	arrows: false,
-	focusOnSelect: true,
-	responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 640,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1
-			}
-		}
-	]
-	});
-
-	// testimonial carousel
-	$('.testimonial-carousel').slick({
-		dots: true,
-		fade:true
-	});
-
-
 });
 
 /* Script on load
 ------------------------------------------------------------------------------*/
 $(window).on('load', function () {
 	// page is fully loaded, including all frames, objects and images
-	
-	// explore gallery
-	if ($('.tabbed-gallery').length > 0) {
-		$('.tg_grid').isotope({
-			itemSelector: '.tg_grid-item'
-		});
-	
-		$('.tg_tab-switch li').click(function () {
-			$('.tg_tab-switch li.active').removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$('.tg_grid').isotope({
-				filter: selector
-			})
-			return false;
-		});
-	
-		var result;
-		var filterValue;
-		var $grid = $(".tg_grid").isotope({
-			itemSelector: ".tg_grid-item",
-			filter: function () {
-				var $this = $(this);
-				var searchResult = result ? $this.text().match(result) : true;
-				var selectResult = filterValue ? $this.is(filterValue) : true;
-				return searchResult && selectResult;
-			}
-		});
-	
-		$(".tg_tab-switch-select").on("change", function() {
-			filterValue = $(this).val();
-			console.log(filterValue);
-			$grid.isotope();
-		});
-	}
 
 });
 
